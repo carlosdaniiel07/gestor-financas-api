aee09e9cf28c8293cbc19d87c8aea9569f2e1e7c
Carlos Almeida <carlosdaniiel0711@hotmail.com>
2019-07-06 10:55:04 -0300

    Criação do projeto

---
+++ src/main/java/com/carlos/gestorfinancas/GestorFinancasApiApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.gestorfinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorFinancasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorFinancasApiApplication.class, args);
+	}
+
+}
