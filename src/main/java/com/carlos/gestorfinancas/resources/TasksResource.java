d9b885e97500272f09acb5f2234a985363cf39d8
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2019-10-05 19:08:33 -0300

    Rotina para atualização de movimentos bancários. Issue #22

---
+++ src/main/java/com/carlos/gestorfinancas/resources/TasksResource.java
@@ -0,0 +1,27 @@
+package com.carlos.gestorfinancas.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carlos.gestorfinancas.services.TaskService;
+
+/**
+ * @author Carlos Daniel Martins de Almeida
+ * @date 05/10/2019
+ */
+@RestController
+@RequestMapping(value = "/tasks")
+public class TasksResource {
+	
+	@Autowired
+	private TaskService service;
+	
+	@PostMapping(value = "/atualiza-status-movimentos")
+	public ResponseEntity<Void> atualizaStatusMovimentos() {
+		service.atualizaStatusMovimentos();
+		return ResponseEntity.ok(null);
+	}
+}
---
+++ src/main/java/com/carlos/gestorfinancas/services/TaskService.java
@@ -0,0 +1,45 @@
+package com.carlos.gestorfinancas.services;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.carlos.gestorfinancas.entities.Movimento;
+import com.carlos.gestorfinancas.entities.enums.StatusMovimento;
+import com.carlos.gestorfinancas.utils.DateUtils;
+
+/**
+ * @author Carlos Daniel Martins de Almeida
+ * @date 05/10/2019
+ */
+@Service
+public class TaskService {
+
+	@Autowired
+	private MovimentoService movimentoService;
+	
+	/**
+	 * Atualiza o status dos movimentos agendados. Todos os movimentos agendados (status = StatusMovimento.AGENDADO) 
+	 * com data de contabilização inferior ou igual a data atual terão o seu status atualizado para EFETIVADO (status = StatusMovimento.EFETIVADO)
+	 */
+	public void atualizaStatusMovimentos() {
+		Date dataAtual = DateUtils.getDataAtual();
+		List<Movimento> movimentosAgendados = movimentoService.getAllByStatus(StatusMovimento.AGENDADO);
+		List<Movimento> movimentosParaAtualizar = new ArrayList<Movimento>();
+		
+		movimentosAgendados.forEach((Movimento movimento) -> {
+			Date dataContabilizacao = movimento.getDataContabilizacao();
+			
+			// Caso a data de contabilização seja inferior ou igual a data atual
+			if(dataContabilizacao.compareTo(dataAtual) == 0 || dataContabilizacao.compareTo(dataAtual) < 0) {
+				movimento.setStatus(StatusMovimento.EFETIVADO);
+				movimentosParaAtualizar.add(movimento);
+			}
+		});
+		
+		movimentoService.atualiza(movimentosParaAtualizar);
+	}
+}
