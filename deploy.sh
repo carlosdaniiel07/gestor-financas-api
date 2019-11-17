d53d6be2757e55fc9d988d24aa56ab3eca92a6bc
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2019-11-17 18:15:33 -0300

    Possível correção arquivo deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ NEW_FILE="api-gestor-financas.war"
 cd target
 mv $OLD_FILE $NEW_FILE
 
-curl -T $NEW_FILE -u $USER:$PASSWD ftp://$HOST:$PORT
+curl --verbose --progress-bar --ipv4 --connect-timeout 8 --max-time 120 --retry 128 --ftp-ssl --disable-epsv --ftp-pasv -T $NEW_FILE -u $USER:$PASSWD ftp://$HOST:$PORT
 
 exit 0
