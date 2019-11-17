7fd4ca12ec30b99f8cec38a3d77500880cba1bd0
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2019-11-17 13:41:31 -0300

    Melhorias de desempenho endpoint /subcategorias. Issue #33

--- src/main/java/com/carlos/gestorfinancas/repositories/SubcategoriaRepository.java
+++ src/main/java/com/carlos/gestorfinancas/repositories/SubcategoriaRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.carlos.gestorfinancas.entities.Subcategoria;
@@ -15,7 +16,10 @@ import com.carlos.gestorfinancas.entities.Subcategoria;
  */
 @Repository
 public interface SubcategoriaRepository extends JpaRepository<Subcategoria, Long> {
+	
+	@Query(value = "SELECT s FROM Subcategoria s JOIN FETCH s.categoria WHERE s.ativo = ?1")
 	List<Subcategoria> findByAtivo(boolean ativo);
+	
 	List<Subcategoria> findByAtivo(boolean ativo, Pageable paginacao);
 	
 	Optional<Subcategoria> findByIdAndAtivo(Long id, boolean ativo);
