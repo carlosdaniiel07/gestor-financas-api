4a4e2ee87d70d71220603e980048768deddcdfc3
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-01-01 21:08:30 -0300

    Endpoint p/ atualização de investimento e ajuste de validação na inserção do resgate

--- src/main/java/com/carlos/gestorfinancas/services/InvestimentoService.java
+++ src/main/java/com/carlos/gestorfinancas/services/InvestimentoService.java
@@ -57,6 +57,10 @@ public class InvestimentoService {
 		return investimento;
 	}
 	
+	public void atualiza(Investimento investimento) {
+		repository.save(investimento);
+	}
+	
 	/**
 	 * Registra um reinvestimento para um investimento já existente
 	 * @return
@@ -97,7 +101,7 @@ public class InvestimentoService {
 	 */
 	@Transactional(propagation = Propagation.REQUIRED)
 	public Investimento addResgate(Investimento investimento, ItemInvestimento item) {
-		if (item.getValor() <= investimento.getValorAtual()) {
+		if (item.getValor() <= investimento.getValorAplicado()) {
 			List<ItemInvestimento> itensInvestimento = investimento.getItens();
 			
 			item.setInvestimento(investimento);
