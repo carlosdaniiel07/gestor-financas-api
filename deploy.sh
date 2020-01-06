4cbdaca4954069e4be59f8159834dc449298f4e6
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-01-05 22:55:18 -0300

    Corrigindo deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ NEW_FILE="api-gestor-financas.war"
 cd target
 mv $OLD_FILE $NEW_FILE
 
-if [$TRAVIS_BRANCH == "master"]; then
+if [ $TRAVIS_BRANCH == "master" ]; then
 	curl --verbose -T $NEW_FILE -u $USER:$PASSWD ftp://$HOST:$PORT
 fi
 
