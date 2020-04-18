6048a7afbba4096b9e1dda77524006403da531e7
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-04-18 20:11:01 -0300

    Removido import desnecessário

--- src/main/java/com/carlos/gestorfinancas/GestorFinancasApiApplication.java
+++ src/main/java/com/carlos/gestorfinancas/GestorFinancasApiApplication.java
@@ -1,18 +1,12 @@
 package com.carlos.gestorfinancas;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import com.carlos.gestorfinancas.services.NotificacaoService;
-
 @SpringBootApplication
 public class GestorFinancasApiApplication extends SpringBootServletInitializer implements CommandLineRunner {
-	
-	@Autowired
-	private NotificacaoService notificacaoService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GestorFinancasApiApplication.class, args);
